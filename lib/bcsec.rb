3b52f23aa3dfdc4563825431de62676afd663efb
Rhett Sutphin <rhett@detailedbalance.net>
2010-03-26 20:34:02 -0500

    Add Bcsec.configure entry point for the application's single configuration instance.  Part of #2358.

--- lib/bcsec.rb
+++ lib/bcsec.rb
@@ -1,3 +1,12 @@
 module Bcsec
   VERSION = File.read(File.expand_path('../../VERSION', __FILE__)).strip
+
+  class << self
+    attr_accessor :configuration
+
+    def configure(&block)
+      @configuration ||= Bcsec::Configuration.new
+      @configuration.enhance(&block)
+    end
+  end
 end
--- spec/bcsec_spec.rb
+++ spec/bcsec_spec.rb
@@ -3,6 +3,10 @@ require File.expand_path('../spec_helper', __FILE__)
 require 'bcsec'
 
 describe Bcsec do
+  before do
+    Bcsec.configuration = nil
+  end
+
   describe "::VERSION" do
     it "exists" do
       lambda { Bcsec::VERSION }.should_not raise_error
@@ -10,6 +14,31 @@ describe Bcsec do
 
     it "has three or four dot-separated parts" do
       Bcsec::VERSION.split('.').size.should be_between(3, 4)
+    end
+  end
+
+  describe "configuration" do
+    it "can be set directly" do
+      Bcsec.configuration = Bcsec::Configuration.new { portal :ENU }
+      Bcsec.configuration.portal.should == :ENU
+    end
+
+    it "does not accumulate direct set configurations" do
+      Bcsec.configuration = Bcsec::Configuration.new { api_mode :basic }
+      Bcsec.configuration = Bcsec::Configuration.new { portal :ENU }
+      Bcsec.configuration.api_modes.should == [] # the default
+    end
+
+    it "can be configured using #configure" do
+      Bcsec.configure { portal :ENU }
+      Bcsec.configuration.portal.should == :ENU
+    end
+
+    it "accumulates #configure configurations" do
+      Bcsec.configure { api_modes :basic, :cas_proxy }
+      Bcsec.configure { portal :LIMS }
+      Bcsec.configuration.api_modes.should == [:basic, :cas_proxy]
+      Bcsec.configuration.portal.should == :LIMS
     end
   end
 end
