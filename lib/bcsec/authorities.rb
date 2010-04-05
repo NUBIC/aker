9c9dbb9d05cb8531a9dca66968157e43adfe8862
Rhett Sutphin <r-sutphin@northwestern.edu>
2010-04-05 11:36:51 -0500

    Whitespace

--- lib/bcsec/authorities.rb
+++ lib/bcsec/authorities.rb
@@ -3,7 +3,7 @@ require 'bcsec'
 module Bcsec
   module Authorities
     autoload :AllAccess, 'bcsec/authorities/all_access'
-    autoload :Netid, 'bcsec/authorities/netid'
-    autoload :Static, 'bcsec/authorities/static'
+    autoload :Netid,     'bcsec/authorities/netid'
+    autoload :Static,    'bcsec/authorities/static'
   end
 end
