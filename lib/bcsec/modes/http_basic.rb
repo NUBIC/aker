683e068a71dcea7ac44955254392a3da431f8895
David Yip <yipdw@northwestern.edu>
2010-04-14 12:19:58 -0500

    Filled out HTTP Basic mode.  Closes #2369.

--- lib/bcsec/modes/http_basic.rb
+++ lib/bcsec/modes/http_basic.rb
@@ -1,4 +1,5 @@
 require 'bcsec'
+require 'base64'
 
 module Bcsec
   module Modes
@@ -10,10 +11,91 @@ module Bcsec
     # Basic challenge is present.  It operates interactively when it is
     # configured as an interactive authentication mode.
     #
+    # @see http://www.ietf.org/rfc/rfc2617.txt
+    #      RFC 2617
     # @author David Yip
     class HttpBasic < Bcsec::Modes::Base
+      ##
+      # Recognizes valid Basic challenges.
+      #
+      # An HTTP Basic challenge is the word "Basic", followed by one space,
+      # followed by a Base64-encoded string.
+      #
+      # @see http://www.ietf.org/rfc/rfc2045.txt
+      #      RFC 2045, section 6.8
+      BasicPattern = %r{^Basic ((?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?)$}
+
+      ##
+      # The authentication realm to be used in challenges.
+      attr_accessor :realm
+
+      ##
+      # A key that refers to this mode; used for configuration convenience.
+      #
+      # @return [Symbol]
       def self.key
         :http_basic
+      end
+
+      ##
+      # Authenticates a (username, password) pair.
+      #
+      # If authentication is successful, then success! (from
+      # Warden::Strategies::Base) is called with a Bcsec::User object.  If
+      # authentication fails, then nothing is done.
+      #
+      # @return [nil]
+      def authenticate!
+        user = authority.valid_credentials?(:user, *credentials)
+        success!(user) if user
+      end
+
+      ##
+      # Builds a Rack response with status 401 that indicates a need for
+      # authentication.
+      #
+      # With Web browsers, this will cause a username/password dialog to
+      # appear.
+      #
+      # @return [Rack::Response]
+      def on_ui_failure(env)
+        Rack::Response.new([], 401, {'WWW-Authenticate' => scheme})
+      end
+
+      ##
+      # Used to build a WWW-Authenticate header that will be returned to a
+      # client failing noninteractive authentication.
+      #
+      # @return [String]
+      def scheme
+        %Q{Basic realm="#{realm}"}
+      end
+
+      ##
+      # Returns true if a valid Basic challenge is present, false otherwise.
+      def valid?
+        env['HTTP_AUTHORIZATION'] =~ BasicPattern
+      end
+
+      private
+
+      ##
+      # Decodes and extracts a (username, password) pair from a Basic challenge.
+      #
+      # @return [Array<String>] username and password, or an empty array if the
+      #         encoded credentials do not conform to the Basic Authentication
+      #         Scheme
+      #
+      # @see http://www.ietf.org/rfc/rfc2617.txt
+      #      RFC 2617, section 2
+      def credentials
+        encoded_credentials = env['HTTP_AUTHORIZATION'].match(BasicPattern)
+
+        if encoded_credentials && encoded_credentials[1]
+          Base64.decode64(encoded_credentials[1]).split(':', 2)
+        else
+          []
+        end
       end
     end
   end
--- spec/bcsec/modes/http_basic_spec.rb
+++ spec/bcsec/modes/http_basic_spec.rb
@@ -1,10 +1,11 @@
 require File.expand_path("../../../spec_helper", __FILE__)
 require 'rack'
+require 'base64'
 
 module Bcsec::Modes
   describe HttpBasic do
     before do
-      @env = Rack::MockRequest.env_for('/')
+      @env = Rack::MockRequest.env_for("/")
       @scope = mock
       @mode = HttpBasic.new(@env, @scope)
     end
@@ -15,18 +16,75 @@ module Bcsec::Modes
       end
     end
 
-    describe "#valid?"
+    describe "#valid?" do
+      it "is not valid if the Authorization header is blank" do
+        @mode.should_not be_valid
+      end
 
-    describe "#scheme" do
-      it "returns Basic with a realm" do
-        @mode.realm = 'Realm'
+      it "is not valid if the Authorization header does not contain 'Basic'" do
+        @env["HTTP_AUTHORIZATION"] = "Fake auth"
 
-        @mode.scheme.should == "Basic realm=Realm"
+        @mode.should_not be_valid
+      end
+
+      it "is not valid if the Authorization header contains malformed credentials" do
+        @env["HTTP_AUTHORIZATION"] = "Basic :?$"
+
+        @mode.should_not be_valid
+      end
+
+      it "is valid if the Authorization header contains 'Basic' followed by base64-encoded credentials" do
+        credentials = Base64.encode64("foo:bar")
+        @env["HTTP_AUTHORIZATION"] = "Basic #{credentials}"
+
+        @mode.should be_valid
       end
     end
 
-    describe "#authenticate!"
+    describe "#scheme" do
+      it "returns Basic with a realm" do
+        @mode.realm = "Realm"
 
-    describe "#on_ui_failure"
+        @mode.scheme.should == %q{Basic realm="Realm"}
+      end
+    end
+
+    describe "#authenticate!" do
+      before do
+        @authority = mock
+        @mode.stub!(:authority => @authority)
+        @env["HTTP_AUTHORIZATION"] = "Basic " + Base64.encode64("foo:bar")
+      end
+
+      it "signals success if the username and password are good" do
+        user = stub
+        @authority.should_receive(:valid_credentials?).with(:user, 'foo', 'bar').and_return(user)
+        @mode.should_receive(:success!).with(user)
+
+        @mode.authenticate!
+      end
+
+      it "does not signal success if the username or password are bad" do
+        @authority.stub(:valid_credentials? => nil)
+        @mode.should_not_receive(:success!)
+
+        @mode.authenticate!
+      end
+    end
+
+    describe "#on_ui_failure" do
+      it "returns 401 Unauthorized" do
+        response = @mode.on_ui_failure(@env)
+
+        response.status.should == 401
+      end
+
+      it "returns a WWW-Authenticate header containing the Basic authentication scheme" do
+        @mode.realm = "Realm"
+
+        response = @mode.on_ui_failure(@env)
+        response.headers['WWW-Authenticate'].should == %q{Basic realm="Realm"}
+      end
+    end
   end
 end
