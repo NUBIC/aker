3977b9557686a29fa777d25a533f6161ed9ab238
David Yip <yipdw@northwestern.edu>
2010-04-15 13:26:04 -0500

    Dropped API key authentication mode.  Closes #2371.

--- lib/bcsec/modes.rb
+++ lib/bcsec/modes.rb
@@ -2,7 +2,6 @@ require 'bcsec'
 
 module Bcsec
   module Modes
-    autoload :ApiKey, 'bcsec/modes/api_key'
     autoload :Base, 'bcsec/modes/base'
     autoload :Cas, 'bcsec/modes/cas'
     autoload :CasProxy, 'bcsec/modes/cas_proxy'
