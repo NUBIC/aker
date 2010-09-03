64ddd7cbc7fab1b4b7444c4ea3894307270c686c
Rhett Sutphin <r-sutphin@northwestern.edu>
2010-09-03 11:22:14 -0500

    Update to ree-2010.02.

--- ci-rvm.sh
+++ ci-rvm.sh
@@ -21,7 +21,7 @@ BCSEC_JRUBY='jruby-1.4.0'
 unset BCSEC_RVM_RUBY
 case "$BCSEC_ENV" in
 'ci_1.8.7')
-BCSEC_RVM_RUBY='ree-1.8.7-2010.01';
+BCSEC_RVM_RUBY='ree-1.8.7-2010.02';
 ;;
 'ci_1.9.1')
 BCSEC_RVM_RUBY='ruby-1.9.1-p378';
