b94aa5a10877fd086207c4f41c21b88020254213
Rhett Sutphin <r-sutphin@northwestern.edu>
2010-11-08 12:01:36 -0600

    Ensure that none of the pers models can be saved without audit info set.  #3836.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,7 @@ require 'rspec'
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 
 require 'bcsec'
+require 'bcaudit'
 
 require File.expand_path('../database_helper', __FILE__)
 require File.expand_path('../deprecation_helper', __FILE__)
@@ -20,6 +21,10 @@ RSpec.configure do |config|
   Bcsec::Spec::DatabaseData.use_in(config)
   Bcsec::Spec::DeprecationMode.use_in(config)
   config.include Bcsec::Spec::LoggerHelper
+
+  config.before do
+    Bcaudit::AuditInfo.current_user = Bcsec::User.new("spec-runner").tap { |u| u.personnel_id = 42 }
+  end
 end
 
 def port_offset
