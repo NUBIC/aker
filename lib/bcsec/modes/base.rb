031c76be1a7c9b58495aead6c00536ed8386e958
David Yip <yipdw@northwestern.edu>
2011-02-11 12:44:25 -0600

    Removed references to Warden v0.10.3; made recommendation to subclass Bcsec::Modes::Base more concise.  #2757.
    I figure that if someone ends up with a real problem subclassing
    Bcsec::Modes::Base, we can come up with a solution at that time.

--- lib/bcsec/modes/base.rb
+++ lib/bcsec/modes/base.rb
@@ -11,23 +11,11 @@ module Bcsec
     # require input from a human, and/or _non-interactive_, meaning that they
     # can be used without user intervention.
     #
-    # For mode implementors: It is not strictly necessary to implement bcsec
-    # authentication modes as subclasses of `Bcsec::Modes::Base`.  However, keep
-    # in mind the following:
-    #
-    # * Your mode must at some point in its inheritance hierarchy subclass
-    #   `Warden::Strategies::Base`, because that's what bcsec uses internally to
-    #   implement authentication, and Warden actually checks subtyping.
-    #   `Bcsec::Modes::Base` sets up the inheritance hierarchy for you.
-    # * `Bcsec::Modes::Base` implements functionality shared across modes.
+    # For mode implementors: While it is not strictly necessary to implement
+    # bcsec modes as subclasses of `Bcsec::Modes::Base`, it is recommended that
+    # you do so.
     #
     # @author David Yip
-    # @see
-    #  http://github.com/hassox/warden/blob/v0.10.3/lib/warden/strategies/base.rb
-    #  Warden::Strategies::Base at hassox:warden@v0.10.3
-    # @see
-    #  http://github.com/hassox/warden/blob/v0.10.3/lib/warden/strategies.rb#L14-16
-    #  Warden::Strategies at hassox:warden@v0.10.3
     class Base < Warden::Strategies::Base
       ##
       # Exposes the configuration this mode should use.
