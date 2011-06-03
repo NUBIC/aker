25d1937089695c867936ba53bc3b066b2c442350
David Yip <yipdw@member.fsf.org>
2011-06-03 12:06:27 -0500

    Shallower indentation.  #5469.

--- lib/bcsec/modes/middleware/form.rb
+++ lib/bcsec/modes/middleware/form.rb
@@ -1,14 +1,10 @@
 require 'bcsec'
 
-module Bcsec
-  module Modes
-    module Middleware
-      module Form
-        autoload :ConfigurationHelper,  'bcsec/modes/middleware/form/configuration_helper'
-        autoload :LogoutResponder,      'bcsec/modes/middleware/form/logout_responder'
-        autoload :LoginRenderer,        'bcsec/modes/middleware/form/login_renderer'
-        autoload :LoginResponder,       'bcsec/modes/middleware/form/login_responder'
-      end
-    end
+module Bcsec::Modes::Middleware
+  module Form
+    autoload :ConfigurationHelper,  'bcsec/modes/middleware/form/configuration_helper'
+    autoload :LogoutResponder,      'bcsec/modes/middleware/form/logout_responder'
+    autoload :LoginRenderer,        'bcsec/modes/middleware/form/login_renderer'
+    autoload :LoginResponder,       'bcsec/modes/middleware/form/login_responder'
   end
 end
